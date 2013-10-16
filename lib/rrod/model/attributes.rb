950700f2cbaa9a88b52ce2e89c6a950c4f98c457
Adam Hunter <adamhunter@me.com>
2013-10-16 18:39:12 -0400

    adding documentation to attributes module

--- lib/rrod/model/attributes.rb
+++ lib/rrod/model/attributes.rb
@@ -16,23 +16,33 @@ module Rrod
         write_attribute :id, value
       end
 
+      # Returns a new hash with all of the object's attributes.
+      # @return [Hash] the object's attributes
       def attributes
         @attributes.keys.inject({}) { |acc, key|
           acc.tap { |hash| hash[key] = respond_to?(key) ? public_send(key) : nil }
         }
       end
 
+      # Mass assign the attributes of the object.
+      # @param [Hash] the attributes to mass assign
       def attributes=(attrs)
         attrs.each do |key, value|
           public_send "#{key}=", value
         end
       end
 
+      # Read a single attribute of the object.
+      # @param [Symbol, String] the key of the attribute to read
+      # @return the attribute at the given key
       def read_attribute(key)
         @attributes[key.to_s]
       end
       alias :[] :read_attribute
 
+      # Write a given value to the attributes for a given key.
+      # @param [Symbol, String] the key of the attribute to write
+      # @param the value to write to attributes
       def write_attribute(key, value)
         @attributes[key.to_s] = value
       end
